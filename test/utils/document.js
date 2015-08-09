0440029686b3248eadc4754e4aafa2c5c147eb07
Michael Chan <mijoch@gmail.com>
2015-08-09 08:38:52 -0700

    * add jsdom and build out basic testing document
    For testing with a DOM, we need (well), a DOM. We use JSDOM and build up `document.js` with on honest-to-goodness document.
    
    * add package `jsdom`
    * add DOM smarts to `document.js`

--- test/utils/document.js
+++ test/utils/document.js
@@ -1,1 +1,20 @@
-global.document = {};
+// Taken from: http://jaketrent.com/post/testing-react-with-jsdom/
+var jsdom = require("jsdom");
+
+var doc = jsdom.jsdom("<!doctype html><html><body></body></html>");
+
+var win = doc.defaultView;
+
+global.document = doc;
+global.window = win;
+
+propagateToGlobal(win);
+
+function propagateToGlobal (window) {
+  for (let key in window) {
+    if (!window.hasOwnProperty(key)) continue;
+    if (key in global) continue;
+
+    global[key] = window[key];
+  }
+}
