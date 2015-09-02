eb49f525063f2bf1ddcd686adad69a2664e22ce5
Michael Chan <mijoch@gmail.com>
2015-09-02 15:16:10 -0700

    test(*): add Bd-test, Img-test, and ImgExt-test files

---
+++ test/Img-test.js
@@ -0,0 +1,51 @@
+import assert from "assert";
+import sinon from "sinon";
+
+import React, { addons } from "react/addons";
+import Img from "../src/Img.js";
+
+let shallowRenderer = addons.TestUtils.createRenderer();
+
+describe("Img", () => {
+  let result;
+
+  before(() => {
+    shallowRenderer.render(<Img href=""><div /></Img>);
+    result = shallowRenderer.getRenderOutput();
+  });
+
+  it("renders an `a` as it's root element", () => {
+    assert.strictEqual(result.type, "a");
+  });
+
+  it("renders with the `className` img", () => {
+    assert.strictEqual(result.props.className, "img");
+  });
+
+  it("element has default styles", () => {
+    assert.strictEqual(result.props.style.float, "left");
+    assert.strictEqual(result.props.style.marginRight, 10);
+  });
+
+  /*
+   * FIX:
+   * Something is preventing doing a camparison with JSX. Find out why.
+   * Example: https://github.com/facebook/react/blob/4b9c349fd057b3c4b1a0fab5fbdf5bd0c625db8d/src/test/__tests__/ReactTestUtils-test.js#L179-L195
+   */
+  it("renders `props.children`", () => {
+    shallowRenderer.render(<Img href="">hi</Img>);
+    result = shallowRenderer.getRenderOutput();
+
+    assert.strictEqual(result.props.children, "hi");
+  });
+
+  it("accepts event props", () => {
+    let spyCallback = sinon.spy();
+
+    shallowRenderer.render(<Img href="" onClick={spyCallback}><div /></Img>);
+    result = shallowRenderer.getRenderOutput();
+    result.props.onClick();
+
+    assert.strictEqual(spyCallback.calledOnce, true);
+  });
+});
